ef06a4e5069c12cd3e5d48ad7432b3bf800678bd
enas48 <enasm4829@gmail.com>
2021-12-05 19:24:26 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import {handleInitialData} from '../actions/shared'
+import {connect} from 'react-redux'
+import Dashboard from './Dashboard';
+class App extends Component {
+  componentDidMount(){
+    this.props.dispatch(handleInitialData())
+  }
+  render() {
+    return (
+      <div>
+           {this.props.loading === true
+          ? null
+          : <Dashboard />}
+      </div>
+    )
+  }
+}
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+export default connect(mapStateToProps)(App)
