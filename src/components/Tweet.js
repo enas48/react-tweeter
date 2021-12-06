809e0a22249476133e64f68e26e6fb0504a6d56a
enas48 <enasm4829@gmail.com>
2021-12-06 13:30:04 +0200

    add tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import {formatTweet} from '../utils/helpers'
+import {formatTweet, formatDate} from '../utils/helpers'
+import { TiArrowBackOutline } from "react-icons/ti";
+import { TiHeartOutline } from "react-icons/ti";
+import { TiHeartFullOutline} from "react-icons/ti";
 class Tweet extends Component {
+handleLike=(e)=>{
+    e.preventDefault();
+
+  }
+  toParent=(e, id)=>{
+    e.preventDefault();
+    //redirect to parent tweet
+  }
 
   render() {
-    console.log(this.props)
+const {tweet} = this.props;
+const {
+  name, avatar, timestamp, text, hasLiked, likes, replies, parent
+} = tweet
+if(tweet=== null){
+  return <p>this tweet doesn't exist</p>
+}
     return (
         <div className="tweet">
-     
+          <img src={avatar} alt={`Avatar of ${name}`} className="avatar" />
+      <div className="tweet-info">
+        <div>
+        <span>{name}</span>
+        <div>{formatDate(timestamp)}</div>
+        {parent &&(
+          <button className="replying-to" onClick={(e)=>this.toParent(e, parent.id)}>
+            Replying to @{parent.author}
+          </button>
+        )}
+        <p>{text}</p>
+        </div>
+        <div className="tweet-icons">
+          <TiArrowBackOutline className="tweet-icon"/>
+          <span>{replies !==0 && replies}</span>
+          <button className="heart-button" onClick={this.handleLike}>
+            {hasLiked === true? <TiHeartFullOutline color="#e0245e" className="tweet-icon"/>:
+            <TiHeartOutline className="tweet-icon"/>}
+          </button>
+          <span>{likes !==0 && likes}</span>
+      </div>
+      
+      </div>
+   
       </div>
     )
   }
@@ -15,10 +55,12 @@ class Tweet extends Component {
 
 function mapStateToProps( {authedUser, users, tweets},{id} ){
     const tweet=tweets[id];
-    const parentTweet= tweets[tweet.replyingTo]
+    const parentTweet= tweet ? tweets[tweet.replyingTo] : null;
     return {
         authedUser,
-        tweet: formatTweet(tweet, users[tweet.author], authedUser)
+        tweet: tweet
+        ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+        : null
     }
   }
 
